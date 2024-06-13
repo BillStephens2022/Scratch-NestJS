89986257ae7770f470aa97789e07e02ba4b57070
BillStephens2022 <two4onebill@yahoo.com>
2024-06-13 06:41:26 -0400

    updated html returned on get route, and added some basic styling

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,23 @@
-import { Controller, Module, Get } from "@nestjs/common"
+import { Controller, Module, Get } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
-
 
 @Controller()
 class AppController {
-    @Get()
-    getRootRoute() {
-        return "<h1>Hi There</h1>";
-    }
+  @Get()
+  getRootRoute() {
+    return '<div style="background: black; min-height: 100vh; display: flex; flex-direction: column; justify-content: center; align-items: center;"><h1 style="font-size: 3rem; color: steelblue;">Hi there, Bill</h1><h2 style="font-size: 1.75rem; color: white;">Welcome to your very first NestJS app!!!</h1></div>';
+  }
 }
 
 @Module({
-    controllers: [AppController]
+  controllers: [AppController],
 })
 class AppModule {}
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule);
 
-    await app.listen(3000);
+  await app.listen(3000);
 }
 
 bootstrap();
